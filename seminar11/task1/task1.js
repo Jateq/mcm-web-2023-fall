069deae910a72c055cd991d2ef5215dd1f992df6
jateq <temirlan.eraly1@gmail.com>
2023-11-23 13:33:26 +0600

    task1 animation, finished

--- seminar11/task1/task1.js
+++ seminar11/task1/task1.js
@@ -1,25 +1,11 @@
-// Function to update the loading indicator text
-function updateLoadingIndicator() {
-    const indicator = document.getElementById('loading-indicator');
-    indicator.innerText += '.';
-}
-
 // Function to simulate loading process
 function simulateLoading() {
-    // Simulate a task that takes time to complete
-    setTimeout(function () {
-        updateLoadingIndicator();
-        // Check if loading is complete (you can replace this condition with your own logic)
-        if (document.readyState === 'complete') {
-            document.getElementById('loading-indicator').innerText = 'Loading Complete';
-        } else {
-            // Continue simulating loading
-            simulateLoading();
-        }
-    }, 100); // Update every 100 milliseconds
+    const loadingContainer = document.getElementById('loading-container');
+        setTimeout(function () {
+            document.getElementById('text').innerText = 'Loading finished';
+            loadingContainer.style.backgroundColor = '#ba79f1';
+        }, 1000);
 }
-
-// Start the loading simulation when the page loads
 window.onload = function () {
     simulateLoading();
 };
